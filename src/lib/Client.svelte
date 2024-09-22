b94d1b4fd796d41b50dbd576b43982f3143ada98
DauMoe <lehuyhoang1111999@gmail.com>
2024-09-22 22:57:29 +0700

    Add scss into code base

---
+++ src/lib/Client.svelte
@@ -0,0 +1,9 @@
+<script>
+  import "$lib/styles/_client.scss";
+  import ArrowLeft from "$lib/assets/icons/ArrowLeft.png";
+</script>
+
+<div>
+  <img src={ArrowLeft} alt="Back"/>
+  <p>Iam size 16px</p>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import Client from "$lib/Client.svelte";
+  import "$lib/styles/root.scss";
+</script>
+
+<Client/>
