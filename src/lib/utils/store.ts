183d61bff25375aff89d9ef1eef0aedb7618ea7a
DauMoe <lehuyhoang1111999@gmail.com>
2024-10-13 23:04:40 +0700

    add header mock data

--- src/lib/utils/store.ts
+++ src/lib/utils/store.ts
@@ -2,23 +2,62 @@ import { writable } from "svelte/store";
 
 export const clientStore = writable({
   header: {
-    primary: [{
+    client: [{
+      id: 1,
       title: "store",
-      selected: true
+      selected: true,
+      items: [{
+        id: 1,
+        title: "Home"
+      }, {
+        id: 2,
+        title: "Browser"
+      }, {
+        id: 3,
+        title: "Discover"
+      }, {
+        id: 4,
+        title: "Point shop"
+      }, {
+        id: 5,
+        title: "Curators"
+      }, {
+        id: 6,
+        title: "Gift Cards"
+      }, {
+        id: 7,
+        title: "News"
+      }]
     }, {
+      id: 2,
       title: "community",
-      selected: false
+      selected: false,
+      items: [{
+        id: 1,
+        title: "Home"
+      }, {
+        id: 2,
+        title: "Collections"
+      }, {
+        id: 3,
+        title: "News and Updates"
+      }]
     }, {
+      id: 3,
       title: "$username",
-      selected: false
+      selected: false,
+      items: null
     }, {
+      id: 4,
       title: "library",
-      selected: false
+      selected: false,
+      items: null
     }, {
+      id: 5,
       title: "downloads",
-      selected: false
+      selected: false,
+      items: null
     }],
-    secondary: []
   },
   username: "DauMoe",
   avatarUrl: "",
