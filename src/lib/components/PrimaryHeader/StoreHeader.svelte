7fe359b0449c39e3183ba9012e6bd27afb32aa44
DauMoe <lehuyhoang1111999@gmail.com>
2024-10-13 23:04:58 +0700

    add secondary header

---
+++ src/lib/components/PrimaryHeader/StoreHeader.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+  export let items: Array<any>;
+</script>
---
+++ src/lib/components/PrimaryHeader/index.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '$lib/styles/_primaryHeader.scss';
+	import LibraryHeader from './LibraryHeader.svelte';
+	import StoreHeader from './StoreHeader.svelte';
+	export let selectedItem: { id: number, title: string, items: Array<any> };
+</script>
+
+<div class="primaryHeader">
+	{#if selectedItem}
+		{#if selectedItem.id === 1}
+			<StoreHeader items={selectedItem.items}/>
+		{:else if selectedItem.id === 2}
+			<LibraryHeader />
+		{/if}
+	{/if}
+</div>
