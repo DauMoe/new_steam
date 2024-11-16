3046946a1c15ea7910793fb30a76dfa55ed748d2
DauMoe <lehuyhoang1111999@gmail.com>
2024-11-16 22:45:47 +0700

    Add banner

---
+++ src/lib/components/Banner.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import "$lib/styles/_banner.scss";
+	import { goto } from "$app/navigation";
+	import Clickable from "./Clickable.svelte";
+
+  const baseUrl = __API_BASE_URL__;
+
+  export let primary_url: string | undefined;
+  export let url: string | undefined;
+
+  function gotoUrl() {
+    if (url) goto(url);
+  }
+</script>
+
+{#if primary_url}
+  <Clickable className="banner" onClick={gotoUrl}>
+    <img src={`${baseUrl}/${primary_url}`} alt="banner" />
+  </Clickable>
+{/if}
